5039471ef63d5f9a2b6f9e19e34336ed01676016
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-07 14:36:02 +0530

    updated

--- deployement_1.sh
+++ deployement_1.sh
@@ -146,10 +146,10 @@ done
 
 # -----------------
 ## declare an array variable
-declare -a arr=("element1" "element2" "element3")
+#declare -a arr=("element1" "element2" "element3")
 
 ## now loop through the above array
-for i in "${arr[@]}"
+for i in "${Array3[@]}"
 do
    echo "$i"
    # or do whatever with individual element of the array
