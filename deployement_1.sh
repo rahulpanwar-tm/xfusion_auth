bc1226b6e10f6c2c346e815fc648836d71a75493
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-07 14:34:32 +0530

    updated

--- deployement_1.sh
+++ deployement_1.sh
@@ -144,6 +144,18 @@ do
    # or do whatever with individual element of the array
 done
 
+# -----------------
+## declare an array variable
+declare -a arr=("element1" "element2" "element3")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+done
+# ------------------------
+
 
 #runing code
 #results=($(mysql -udeveloper -padmin@123 -c -h 192.168.1.122 -Bse "show databases;"))
